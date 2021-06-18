94a3ff939a7844b96d7a770c4d7237a2ff9bbd42
dhfhfk <qkrwnsgh1203@gmail.com>
2021-06-18 17:29:10 +0900

    저장 경로 변경

--- main.py
+++ main.py
@@ -8,31 +8,35 @@ package_detail = 'https://dccon.dcinside.com/index/package_detail'
 imgdownl_url = 'https://dcimg5.dcinside.com/dccon.php?no='
 dccon_home = 'https://dccon.dcinside.com/'
 
-dccon_id = input("디시콘 ID= ")
 
-s = requests.Session()
-r = s.get(package_detail, headers=cookie_header)
-req = s.post(package_detail, headers=cookie_header, data={'ci_t':r.cookies['ci_c'], 'package_idx':dccon_id})
+while True: 
+    os.system('cls')
+    dccon_id = input("디시콘 ID= ")
 
-json_data = req.json()
+    s = requests.Session()
+    r = s.get(package_detail, headers=cookie_header)
+    req = s.post(package_detail, headers=cookie_header, data={'ci_t':r.cookies['ci_c'], 'package_idx':dccon_id})
 
-default_dir = os.path.dirname(os.path.abspath(__file__))
-download_path = os.path.join(default_dir, json_data['info']['title'])
-title = json_data['info']['title']
+    json_data = req.json()
 
-try:
-	os.makedirs(download_path)
-except Exception as e:
-	print(e)
-else:
-	for item in tqdm(json_data['detail']):
-		filename = item['idx']+'.'+item['ext']
-		image = s.get(imgdownl_url+item['path'], headers={'Referer': 'https://dccon.dcinside.com/'})
-		with open(os.path.join(download_path, filename), 'wb') as fd:
-			for chunk in image.iter_content(chunk_size=128):
-				fd.write(chunk)
-			fd.close()
-	s.close()
-	print('디시콘: ', title, '(', dccon_id, ')','다운 완료')
+    default_dir = (os.path.abspath("./Downloads"))
+    download_path = os.path.join(default_dir, json_data['info']['title'])
+    title = json_data['info']['title']
 
-os.system("pause")
+    try:
+        os.makedirs(download_path)
+    except Exception as e:
+        print(e)
+    else:
+        for item in tqdm(json_data['detail']):
+            filename = item['idx']+'.'+item['ext']
+            image = s.get(imgdownl_url+item['path'], headers={'Referer': 'https://dccon.dcinside.com/'})
+            with open(os.path.join(download_path, filename), 'wb') as fd:
+                for chunk in image.iter_content(chunk_size=128):
+                    fd.write(chunk)
+                fd.close()
+        s.close()
+        print('다운로드 경로: ', download_path)
+        print('디시콘: ', title, '(', dccon_id, ')','다운 완료')
+
+    os.system("pause")
